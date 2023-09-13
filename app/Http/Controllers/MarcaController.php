e69c87840e1b1959db05d5fe15d52269c257fd80
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2023-09-13 10:03:04 -0300

    v13092023

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -2,70 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Marca;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    public function index()
-    {
-        $marcas = Marca::all();
-        return view('marcas.index', compact('marcas'));
-    }
-
-    public function show($id)
-    {
-        $marca = Marca::find($id);
-        return view('marcas.show', compact('marca'));
-    }
-
-    public function create()
-    {
-        return view('marcas.create');
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'codigo' => 'required',
-            'descricao' => 'required',
-        ]);
-
-        Marca::create([
-            'codigo' => $request->codigo,
-            'descricao' => $request->descricao,
-        ]);
-
-        return redirect()->route('marcas.index')->with('success', 'Marca criada com sucesso.');
-    }
-
-    public function edit($id)
-    {
-        $marca = Marca::find($id);
-        return view('marcas.edit', compact('marca'));
-    }
-
-    public function update(Request $request, $id)
-    {
-        $request->validate([
-            'codigo' => 'required',
-            'descricao' => 'required',
-        ]);
-
-        $marca = Marca::find($id);
-        $marca->update([
-            'codigo' => $request->codigo,
-            'descricao' => $request->descricao,
-        ]);
-
-        return redirect()->route('marcas.index')->with('success', 'Marca atualizada com sucesso.');
-    }
-
-    public function destroy($id)
-    {
-        $marca = Marca::find($id);
-        $marca->delete();
-
-        return redirect()->route('marcas.index')->with('success', 'Marca excluída com sucesso.');
-    }
+    //
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
-
+use App\Http\Controllers\MarcaController;
 use Illuminate\Support\Facades\Route;
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,22 +17,22 @@ use Illuminate\Support\Facades\Route;
 }); */
 
 // Rota para listar todas as marcas
-Route::get('/marcas', 'MarcaController@index')->name('marcas.index');
+Route::get('/marcas', [MarcaController::class, "index"])->name('marcas.index');
 
 // Rota para exibir o formulário de criação de uma nova marca
-Route::get('/marcas/create', 'MarcaController@create')->name('marcas.create');
+Route::get('/marcas/create', [MarcaController::class, "create"])->name('marcas.create');
 
 // Rota para armazenar uma nova marca no banco de dados
-Route::post('/marcas', 'MarcaController@store')->name('marcas.store');
+Route::post('/marcas', [MarcaController::class, "store"])->name('marcas.store');
 
 // Rota para exibir os detalhes de uma marca específica
-Route::get('/marcas/{id}', 'MarcaController@show')->name('marcas.show');
+Route::get('/marcas/{id}', [MarcaController::class, "show"])->name('marcas.show');
 
 // Rota para exibir o formulário de edição de uma marca
-Route::get('/marcas/{id}/edit', 'MarcaController@edit')->name('marcas.edit');
+Route::get('/marcas/{id}/edit', [MarcaController::class, "edit"])->name('marcas.edit');
 
 // Rota para atualizar uma marca no banco de dados
-Route::put('/marcas/{id}', 'MarcaController@update')->name('marcas.update');
+Route::put('/marcas/{id}', [MarcaController::class, "update"])->name('marcas.update');
 
 // Rota para excluir uma marca do banco de dados
-Route::delete('/marcas/{id}', 'MarcaController@destroy')->name('marcas.destroy');
+Route::delete('/marcas/{id}', [MarcaController::class, "destroy"])->name('marcas.destroy');
